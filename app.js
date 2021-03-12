f80e0601113675852d178d3fb5bd2fa5855c9ee6
mario sousa <vegetalizado@gmail.com>
2021-03-12 13:46:20 +0000

    set cookies

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
-require('dotenv').config();
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const cors         = require('cors');
-const session      = require('express-session');
-const passport     = require('passport');
-
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 
 //Include passport configuration
-require('./configs/passport');
+require("./configs/passport");
 mongoose
-  .connect(process.env.MONGODB_URI,
-  {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 const app = express();
 
+app.set("trust proxy", 1);
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.set('trust proxy', 1);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      sameSite: "none", //the requester is on the same domain
+      secure: true, //not using https
+      httpOnly: false, //site on only on http
+      maxAge: 600000, //coolie time to live
+    },
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  saveUninitialized: true,
-  resave: false,
-  cookie: {
-    sameSite: false, //the requester is on the same domain
-    secure: true, //not using https
-    httpOnly: false,//site on only on http
-    maxAge: 600000 //coolie time to live
-  },
-
-/* 
+    /* 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: true,
@@ -54,8 +57,9 @@ app.use(session({
     httpOnly: true, //site on only on http
     maxAge: 600000 //coolie time to live
   }, */
-  rolling: true //session gets refreshed
-}))
+    rolling: true, //session gets refreshed
+  })
+);
 //Initializes passport
 app.use(passport.initialize());
 //Initializes passport session
@@ -65,15 +69,14 @@ app.use(passport.session());
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.CLIENT_HOSTNAME]
+    origin: [process.env.CLIENT_HOSTNAME],
   })
-)
-const index = require('./routes/index');
-app.use('/', index);
-const workoutRoutes = require('./routes/workout-routes');
-app.use('/api', workoutRoutes);
-const authRoutes = require('./routes/auth-routes');
-app.use('/api', authRoutes);
-
+);
+const index = require("./routes/index");
+app.use("/", index);
+const workoutRoutes = require("./routes/workout-routes");
+app.use("/api", workoutRoutes);
+const authRoutes = require("./routes/auth-routes");
+app.use("/api", authRoutes);
 
 module.exports = app;
